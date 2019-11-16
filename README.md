d033161f0262ef4617078665e04bada16da2bf5a
sogrey <suzhaoheng2006@163.com>
2019-11-16 10:11:42 +0800

    deploy master

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-# Cesium-start Example
 Cesium 入门示例
 
 ## 目录
 
 - 快速入门
   - [hello world](https://sogrey.github.io/Cesium-start-Example/hello-world.html)
+
--- deploy.sh
+++ deploy.sh
@@ -25,5 +25,3 @@ git commit -m 'deploy master'
 git push -f https://github.com/Sogrey/Cesium-start-Example.git master
 
 echo -e "\033[32;40m done \033[0m "
-
-cd -
